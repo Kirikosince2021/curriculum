b6e89288f5fc5c3fffa1c191ca0959c8e77ca421
Kirikosince2021 <78951051+Kirikosince2021@users.noreply.github.com>
2021-05-25 09:56:43 +0900

    Delete Java directory

--- Java/CreditCardInfo/src/module-info.java
+++
@@ -1,6 +0,0 @@
-module CreditCardInfo {
-	exports application;
-
-	requires java.desktop;
-	requires jdk.internal.vm.compiler;
-}
--- Java/ImprovementChecker/WebContent/main2.js
+++
@@ -1,5 +0,0 @@
-var today = new Date();
-var YYYY = today.getFullYear();
-var MM = today.getMonth() + 1;
-var DD = today.getDate();
-document.getElementById("date").value = YYYY + "-" + MM + "-" + DD;
